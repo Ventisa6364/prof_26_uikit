55f511819064855c34c62f14de12e167f02897f8
Ventisa6364 <cherkgosa@icloud.com>
2026-02-13 12:32:57 +0300

    добавление первого стиля

--- lib/src/presentation/theme/style.dart
+++ lib/src/presentation/theme/style.dart
@@ -1,7 +1,10 @@
-import 'package:uikit_12_02_26/src/presentation/theme/palette.dart';
+import 'package:flutter/widgets.dart';
+import 'package:uikit_12_02_26/uikit_12_02_26.dart';
 
-class Style{
+class Style {
   final Palette palette;
 
   Style({required this.palette});
+
+  TextStyle get defaultStyle => TextStyle(color: palette.text);
 }
