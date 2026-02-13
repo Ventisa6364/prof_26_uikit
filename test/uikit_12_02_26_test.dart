e7596bb091356322ac58043161fbb7d0e1bbb42f
Ventisa6364 <cherkgosa@icloud.com>
2026-02-13 16:37:37 +0300

    импорт сторонних библиотек

--- test/uikit_12_02_26_test.dart
+++ test/uikit_12_02_26_test.dart
@@ -1,3 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:uikit_12_02_26/uikit_12_02_26.dart';
+// import 'package:flutter_test/flutter_test.dart';
+//
+// import 'package:uikit_12_02_26/uikit_12_02_26.dart';
